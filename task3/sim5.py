4e71392d7d675a7f1db1bddb52ead652053d0a16
cjoh88 <cjoh193@gmail.com>
2016-01-11 23:01:12 +0100

    finished program

--- task3/sim5.py
+++ task3/sim5.py
@@ -9,6 +9,7 @@ import ns.point_to_point
 import ns.flow_monitor
 import time
 import random
+import matplotlib.pyplot as plt
 
 def seed_rng():
     #ns.core.RngSeedManager.SetSeed(int(time.time() * 1000 % (2**31-1)))
@@ -21,16 +22,17 @@ def command_line():
     cmd = ns.core.CommandLine()
 
     # Default values
-    cmd.queue_length = 5
+    cmd.queue_length = 1
     cmd.d_max = 1
     cmd.u_min = 0
-    cmd.u_step = 10
-    cmd.u_max = 4000
-    cmd.latency = 1
+    cmd.u_step = 100
+    cmd.u_max = 1000
+    cmd.latency = 100
     cmd.rate = 500000
-    cmd.error_rate = 0.02
+    cmd.error_rate = 0.2
+    cmd.attempts = 20
 
-    cmd.on_off_rate = 300000
+    cmd.on_off_rate = 300000 #300000
     cmd.AddValue ("rate", "P2P data rate in bps")
     cmd.AddValue ("latency", "P2P link Latency in miliseconds")
     cmd.AddValue ("on_off_rate", "OnOffApplication data sending rate")
@@ -76,6 +78,7 @@ def install_devices(links, data_rate, latency):
     for k, v in links.items():
         s = k.replace("n", "d")
         devices[s] = pointToPoint.Install(v)
+    pointToPoint.EnablePcap("pcap/sim5-uploaders-" + str(len(links) - 3), devices["d2d3"].Get(1), True)
     return devices
 
 def set_error_model(devices, rate):
@@ -157,13 +160,16 @@ def setup_tcp_connection(src_node, dst_node, dst_addr, start_time, stop_time, on
     )
     on_off_tcp_helper.SetAttribute(
         "OnTime",
-        ns.core.StringValue("ns3::ConstantRandomVariable[Constant=2]")
+        #ns.core.StringValue("ns3::ConstantRandomVariable[Constant=2]")
+        ns.core.StringValue("ns3::ExponentialRandomVariable[Mean=2]")
     )
     on_off_tcp_helper.SetAttribute(
         "OffTime",
-        ns.core.StringValue("ns3::ConstantRandomVariable[Constant=1]"))
-    #    ns.core.StringValue("ns3::UniformRandomVariable[Min=1,Max=2]"))
-    #    ns.core.StringValue("ns3::ExponentialRandomVariable[Mean=2]"))
+        #ns.core.StringValue("ns3::ConstantRandomVariable[Constant=1]")
+        #ns.core.StringValue("ns3::UniformRandomVariable[Min=1,Max=2]")
+        ns.core.StringValue("ns3::ExponentialRandomVariable[Mean=1]")
+        )
+
 
     client_apps = on_off_tcp_helper.Install(src_node)
     client_apps.Start(start_time)
@@ -190,6 +196,8 @@ def setup_tcp(nodes, address, no_of_downloaders, no_of_uploaders, on_off_rate):
             ns.core.Seconds(40.0),
             on_off_rate
         )
+
+
 
 def create_flow_monitor():
     helper = ns.flow_monitor.FlowMonitorHelper()
@@ -286,9 +294,78 @@ def sim(no_of_downloaders, no_of_uploaders, cmd):
     destroy()
     return data, ack
 
+def plot(uploaders, througput, packet_loss):
+    plt.figure("Throughput")
+    plt.plot(uploaders, througput)
+    plt.ylabel("Mbits")
+    #plt.show()
+
+    plt.figure("packet_loss")
+    plt.plot(uploaders, packet_loss)
+    plt.ylabel("Avg packets lost")
+    plt.show()
+
 def main():
     cmd = command_line()
-    data, ack = sim(int(cmd.d_max), int(cmd.u_max), cmd)
-    print_result(data)
+    #data, ack = sim(int(cmd.d_max), int(cmd.u_max), cmd)
+    #print_result(data)
+    #sys.exit()
+    #uploaders = list()
+    #th = list()
+    tf = open("througput.r", "a")
+    pf = open("packet_loss.r", "a")
+    throughput_result = list()
+    packet_loss_result = list()
+    uploaders_result = list()
+    for no_uploaders in range(0, int(cmd.u_max), int(cmd.u_step)):
+        #data, ack = sim(int(cmd.d_max), no_uploaders, cmd)
+        #print_result(data)
+        result_data = list()
+        for i in range(0,int(cmd.attempts)):
+            data, ack = sim(int(cmd.d_max), no_uploaders, cmd)
+            result_data.append(data)
+        throughput = 0.0
+        packet_loss = 0.0
+        tf.write("u_" + str(no_uploaders) + " = c(")
+        pf.write("u_" + str(no_uploaders) + " = c(")
+        for i in range(0, len(result_data)):
+            throughput += result_data[i]["throughput"]
+            tf.write(str(result_data[i]["throughput"]) + ", ")
+            packet_loss += result_data[i]["packet_loss"]
+            pf.write(str(result_data[i]["packet_loss"]) + ", ")
+        tf.write(")\n")
+        pf.write(")\n")
+        tf.write("mean_" + str(no_uploaders) + " = mean(u_" + str(no_uploaders) + ")\n")
+        pf.write("mean_" + str(no_uploaders) + " = mean(u_" + str(no_uploaders) + ")\n")
+        tf.write("sd_" + str(no_uploaders) + " = sd(u_" + str(no_uploaders) + ")\n")
+        pf.write("sd_" + str(no_uploaders) + " = sd(u_" + str(no_uploaders) + ")\n")
+        attempts = int(cmd.attempts)
+        #confidence level 0.95
+        tf.write("ce_" + str(no_uploaders) + " = (sd_" + str(no_uploaders) + " / sqrt(" + str(attempts) + ")) * 1.96\n")
+        tf.write("print(\"mean_" + str(no_uploaders) + "\")\n")
+        tf.write("print(mean_" + str(no_uploaders) + ")\n")
+        tf.write("print(\"ce_" + str(no_uploaders) + "\")\n")
+        tf.write("print(ce_" + str(no_uploaders) + ")\n")
+        tf.write("print(\"---------------------------\")\n")
+        pf.write("ce_" + str(no_uploaders) + " = (sd_" + str(no_uploaders) + " / sqrt(" + str(attempts) + ")) * 1.96\n")
+        pf.write("print(\"mean_" + str(no_uploaders) + "\")\n")
+        pf.write("print(mean_" + str(no_uploaders) + ")\n")
+        pf.write("print(\"ce_" + str(no_uploaders) + "\")\n")
+        pf.write("print(ce_" + str(no_uploaders) + ")\n")
+        pf.write("print(\"---------------------------\")\n")
+        print("No of uploaders: " + str(no_uploaders))
+        print(" Avg throughput:  " + str(throughput / len(result_data)))
+        print(" Avg packet loss: " + str(packet_loss / len(result_data)))
+        #result = {
+        #    "uploaders": no_uploaders,
+        #    "throughput": throughput / len(result_data),
+        #    "packet_loss": packet_loss / len(result_data)
+        #}
+        throughput_result.append(throughput / len(result_data))
+        packet_loss_result.append(packet_loss / len(result_data))
+        uploaders_result.append(no_uploaders)
+    tf.close()
+    pf.close()
+    plot(uploaders_result, throughput_result, packet_loss_result)
 
 main()
